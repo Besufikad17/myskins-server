12c5fcb8b4fbeb44d05cecfb6d3b7b82146e8ebd
besufikad17 <besufikadtialhun1994@gmail.com>
2023-03-06 15:53:16 +0300

    bug fixed in error handling

--- src/controllers/skinController.ts
+++ src/controllers/skinController.ts
@@ -1,194 +1,204 @@
-import { Request, Response } from 'express';
-import { PrismaClient } from '@prisma/client'
+import { Request, Response } from "express";
+import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
 class SkinController {
+  public addSkins = async (req: Request, res: Response) => {
+    const { weapon, edition, collectionName, price, img } = req.body;
 
-    public addSkins =async (req: Request, res: Response) => {
-        const { weapon, edition, collectionName, price ,  img} = req.body;
-
-        if(!weapon || !collectionName || !edition || !img || !price){
-            return res.send(401).json({
-                success: false,
-                data: null,
-                error: {msg: "Please enter all fields!"}
-            })
-        }
-       
-        try {
-
-            const skins = await prisma.skins.findMany({
-                where: {
-                    OR: {
-                        collectionName: {
-                            contains: collectionName
-                        }
-                    }
-                }
-            });
-
-            if(skins[0]){
-                res.json({
-                    success: false,
-                    data: null,
-                    error: {msg: "Skin already exists!!"}
-                })
-            }else{
-                const skin = await prisma.skins.create({
-                    data: {
-                     weapon,
-                     collectionName,
-                     edition,
-                     price,
-                     img,
-                     v: 0
-                    }
-                 });
-                 res.json({
-                     success: true,
-                     data: skin,
-                     error: null
-                 })
-            }
-        } catch (error) {
-            res.json({
-                success: false,
-                data: null,
-                error: error
-            })
-        }
+    if (!weapon || !collectionName || !edition || !img || !price) {
+      return res.send(401).json({
+        success: false,
+        data: null,
+        error: { msg: "Please enter all fields!" },
+      });
     }
 
-    public getAllSkins =async (req: Request, res: Response) => {
-        try {
-            const skins = await prisma.skins.findMany({
-                where: {}
-            })
+    try {
+      const skins = await prisma.skins.findMany({
+        where: {
+          OR: {
+            collectionName: {
+              contains: collectionName,
+            },
+          },
+        },
+      });
 
-             res.json({
-                success: true,
-                data: skins,
-                error: null
-            });
-        } catch (error) {
-            res.json({
-                success: false,
-                data: null,
-                error: error
-            });
-        }
+      if (skins[0]) {
+        res.json({
+          success: false,
+          data: null,
+          error: { msg: "Skin already exists!!" },
+        });
+      } else {
+        const skin = await prisma.skins.create({
+          data: {
+            weapon,
+            collectionName,
+            edition,
+            price,
+            img,
+            v: 0,
+          },
+        });
+        res.json({
+          success: true,
+          data: skin,
+          error: null,
+        });
+      }
+    } catch (error: any) {
+      console.log(error);
+      return res.json({
+        success: false,
+        data: null,
+        error: error.meta || { msg: "Error occurred check server log!" },
+      });
+    }
+  };
+
+  public getAllSkins = async (req: Request, res: Response) => {
+    try {
+      const skins = await prisma.skins.findMany({
+        where: {},
+      });
+
+      res.json({
+        success: true,
+        data: skins,
+        error: null,
+      });
+    } catch (error: any) {
+      console.log(error);
+      return res.json({
+        success: false,
+        data: null,
+        error: error.meta || { msg: "Error occurred check server log!" },
+      });
+    }
+  };
+
+  public getSkinsByWeaponType = async (
+    req: Request<{ type: string }>,
+    res: Response
+  ) => {
+    const type: string = req.params.type;
+
+    if (!type) {
+      return res.json({
+        success: false,
+        data: null,
+        error: { msg: "Please enter all fields!!" },
+      });
     }
 
-    public getSkinsByWeaponType = async(req: Request<{ type: string }>, res: Response) => {
-        const type: string = req.params.type;
+    try {
+      const skins = await prisma.skins.findMany({
+        where: {
+          OR: {
+            weapon: {
+              contains: type,
+            },
+          },
+        },
+      });
 
-        if(!type){
-            return res.json({
-                success: false,
-                data: null,
-                error: { msg: "Please enter all fields!!"}
-            });
-        }
+      res.json({
+        success: true,
+        data: skins,
+        error: null,
+      });
+    } catch (error: any) {
+      console.log(error);
+      return res.json({
+        success: false,
+        data: null,
+        error: error.meta || { msg: "Error occurred check server log!" },
+      });
+    }
+  };
 
-        try {
+  public getSkinsByEdition = async (
+    req: Request<{ edition: string }>,
+    res: Response
+  ) => {
+    const edition: string = req.params.edition;
 
-            const skins = await prisma.skins.findMany({
-                where: {
-                    OR: {
-                        weapon: {
-                            contains: type
-                        }
-                    }
-                }
-            })
-
-            res.json({
-                success: true,
-                data: skins,
-                error: null
-            });
-        } catch (error) {
-            res.json({
-                success: false,
-                data: null,
-                error: error
-            });
-        }
+    if (!edition) {
+      return res.json({
+        success: false,
+        data: null,
+        error: { msg: "Please enter all fields!!" },
+      });
     }
 
-    public getSkinsByEdition =async (req:Request<{ edition: string }>, res: Response) => {
-        const edition: string = req.params.edition;
+    try {
+      const skins = await prisma.skins.findMany({
+        where: {
+          OR: {
+            edition: {
+              contains: edition,
+            },
+          },
+        },
+      });
 
-        if(!edition){
-            return res.json({
-                success: false,
-                data: null,
-                error: { msg: "Please enter all fields!!"}
-            });
-        }
+      res.json({
+        success: true,
+        data: skins,
+        error: null,
+      });
+    } catch (error: any) {
+      console.log(error);
+      return res.json({
+        success: false,
+        data: null,
+        error: error.meta || { msg: "Error occurred check server log!" },
+      });
+    }
+  };
 
-        try {
-            const skins = await prisma.skins.findMany({
-                where: {
-                    OR: {
-                        edition: {
-                            contains: edition
-                        }
-                    }
-                }
-            });
+  public getSkinsByCollection = async (
+    req: Request<{ collection: string }>,
+    res: Response
+  ) => {
+    const collection: string = req.params.collection;
 
-            res.json({
-                success: true,
-                data: skins,
-                error: null
-            });
-        } catch (error) {
-            res.json({
-                success: false,
-                data: null,
-                error: error
-            });
-        }
+    if (!collection) {
+      return res.json({
+        success: false,
+        data: null,
+        error: { msg: "Please enter all fields!!" },
+      });
     }
 
-    public getSkinsByCollection =async (req:Request<{ collection: string }>, res: Response) => {
-        const collection: string = req.params.collection;
+    try {
+      const skins = await prisma.skins.findMany({
+        where: {
+          OR: {
+            collectionName: {
+              contains: collection,
+            },
+          },
+        },
+      });
 
-        if(!collection){
-            return res.json({
-                success: false,
-                data: null,
-                error: { msg: "Please enter all fields!!"}
-            });
-        }
-
-        try {
-            const skins = await prisma.skins.findMany({
-                where: {
-                    OR: {
-                        collectionName: {
-                            contains: collection
-                        }
-                    }
-                }
-            });
-
-            res.json({
-                success: true,
-                data: skins,
-                error: null
-            });
-        } catch (error) {
-            res.json({
-                success: false,
-                data: null,
-                error: error
-            });   
-        }
+      res.json({
+        success: true,
+        data: skins,
+        error: null,
+      });
+    } catch (error: any) {
+      console.log(error);
+      return res.json({
+        success: false,
+        data: null,
+        error: error.meta || { msg: "Error occurred check server log!" },
+      });
     }
-
+  };
 }
 
-export { SkinController }
+export { SkinController };
